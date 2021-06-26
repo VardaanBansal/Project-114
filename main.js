9d14fa76602e24b223964ce1ae46bdd4e78e1e38
VardaanBansal <79084613+VardaanBansal@users.noreply.github.com>
2021-06-26 11:42:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup() {
+    canvas = creatCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot()
+{
+    save('My_fillter.png');
+}
